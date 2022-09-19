16c5b18d45466bdd2ac6e55f2ee976954c152d10
mattia-gallinaro <gallinaromattia6@gmail.com>
2022-09-19 19:14:50 +0200

    test con 10 stringhe funzionante

--- Esericizi5F/EsGenerazioneRandom.c
+++ Esericizi5F/EsGenerazioneRandom.c
@@ -3,26 +3,28 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-#define DIM 1000000
+#define tot 9
+#define dim 3
+
 
 int generaLunghezza(){
     int n = 0;
-    n =  rand() % 17 + 3;
-    printf("%d", n);
+    n =  (rand() % 17) + 3;
+    printf("\nGrandezza stringa : %d", n);
     return n;
 }
 
-void generaStringa(char *stringa, int grandezza){
-    stringa = malloc(grandezza);
+char* generaStringa(int grandezza){
+    char *stringa = malloc(grandezza + 1);
     for(int i = 0; i < grandezza; i++){
-        int character = rand() % (126 -  32) + 32; 
+        int character = (rand() % (126 -  32)) + 32; 
         stringa[i] = (char)character;
         if(character == 32 || character == 34 || character == 96){
             i--;
         }
-        //stringa[i] = (char)character;
 
     }
+    return stringa;
     
 }
 
@@ -42,27 +44,27 @@ void scriviSuFile(){
         exit(1);
     }
 
-    for(int i = 0;i <  DIM; i++){
-        fprintf(fp, "%s\n", stringa);
+    for(int i = 0;i <  dim; i++){
+        fprintf(fp, "%s\n", "ciao");
 
     }
 
 }
 int main()
 {
-    char *strs1[330000];
-    char *strs2[330000];
-    char *strs3[340000];
-    int index1, index2, index3;
-    for(int i = 0; i < 1000; i++){
+    char *strs1[dim];
+    char *strs2[dim];
+    char *strs3[dim];
+    int index1, index2, index3 = 0;
+    for(int i = 0; i < tot; i++){
         int n = generaLunghezza();
 
         char *str = generaStringa(n);
 
         
-        swtich(strlen(str)){
+        switch(strlen(str)){
             case 3 ... 8:
-                if(controlloStringhe(strs1, str, index) == 1 || index1 != 0){
+                if(controlloStringhe(strs1, str, index1) == 1 && index1 != dim && index1 != 0){
                     i--;
                 }
                 else{
@@ -71,25 +73,25 @@ int main()
                 }
                 break;
             case 9 ... 14:
-                if(controlloStringhe(strs2, str, index) == 1 || index2 != 0){
+                if(controlloStringhe(strs2, str, index2) == 1 || index2 != dim && index2 != 0 ){
                     i--;
                 }
                 else{
-                    strs1[index2] = str;
+                    strs2[index2] = str;
                     index2++;
                 }
                 break;
             case 15 ... 20:
-                if(controlloStringhe(strs3, str, index) == 1 || index3 != 0){
+                if(controlloStringhe(strs3, str, index3) == 1 && index3 != dim && index3 != 0){
                     i--;
                 }
                 else{
-                    strs1[index3] = str;
+                    strs3[index3] = str;
                     index3++;
                 }
                 break; 
         }
     }
-    FILE
+    //scriviSuFile();
     return 0;
 }
