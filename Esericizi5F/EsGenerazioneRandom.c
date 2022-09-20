7ff7bd36be902d8d4a406b84806935b008589228
mattia-gallinaro <gallinaromattia6@gmail.com>
2022-09-20 18:57:03 +0200

    sistemato

--- Esericizi5F/EsGenerazioneRandom.c
+++ Esericizi5F/EsGenerazioneRandom.c
@@ -10,19 +10,6 @@
 int generaLunghezza()
 {
     int n = 0;
-<<<<<<< HEAD
-    n = rand() % 17 + 3;
-    printf("%d", n);
-    return n;
-}
-
-void generaStringa(char *stringa, int grandezza)
-{
-    stringa = malloc(grandezza);
-    for (int i = 0; i < grandezza; i++)
-    {
-        int character = rand() % (126 - 32) + 32;
-=======
     n =  (rand() % (20- 3)) + 3;
     return n;
 }
@@ -31,21 +18,15 @@ char* generaStringa(int grandezza){
     char *stringa = malloc(grandezza + 1);
     for(int i = 0; i < grandezza; i++){
         int character = (rand() % (126 -  32)) + 32; 
->>>>>>> cebc28051e9ead002755f3caec0f6fcfba90cf29
         stringa[i] = (char)character;
         if (character == 32 || character == 34 || character == 96)
         {
             i--;
         }
-<<<<<<< HEAD
-        // stringa[i] = (char)character;
-    }
-=======
 
     }
     return stringa;
     
->>>>>>> cebc28051e9ead002755f3caec0f6fcfba90cf29
 }
 
 int controlloStringhe(char **strs, char *stringa, int index)
@@ -60,12 +41,7 @@ int controlloStringhe(char **strs, char *stringa, int index)
     return 0;
 }
 
-<<<<<<< HEAD
-void scriviSuFile()
-{
-=======
 void scriviSuFile(char **strs1, char **strs2, char **strs3){
->>>>>>> cebc28051e9ead002755f3caec0f6fcfba90cf29
     FILE *fp;
     if ((fp = fopen("output.txt", "w+")) == NULL)
     {
@@ -73,21 +49,6 @@ void scriviSuFile(char **strs1, char **strs2, char **strs3){
         exit(1);
     }
 
-<<<<<<< HEAD
-    for (int i = 0; i < DIM; i++)
-    {
-        fprintf(fp, "%s\n", stringa);
-    }
-}
-int main()
-{
-    char *strs1[330000];
-    char *strs2[330000];
-    char *strs3[340000];
-    int index1, index2, index3;
-    for (int i = 0; i < 1000; i++)
-    {
-=======
     for(int i = 0;i <  dim; i++){
         fprintf(fp, "%s\n", strs1[i]);
     }
@@ -109,51 +70,10 @@ int main()
     char *strs3[dim];
     int index1, index2, index3 = {0};
     for(int i = 0; i < tot; i++){
->>>>>>> cebc28051e9ead002755f3caec0f6fcfba90cf29
         int n = generaLunghezza();
 
         char *str = generaStringa(n);
 
-<<<<<<< HEAD
-        swtich(strlen(str))
-        {
-        case 3 ... 8:
-            if (controlloStringhe(strs1, str, index) == 1 || index1 != 0)
-            {
-                i--;
-            }
-            else
-            {
-                strs1[index1] = str;
-                index1++;
-            }
-            break;
-        case 9 ... 14:
-            if (controlloStringhe(strs2, str, index) == 1 || index2 != 0)
-            {
-                i--;
-            }
-            else
-            {
-                strs1[index2] = str;
-                index2++;
-            }
-            break;
-        case 15 ... 20:
-            if (controlloStringhe(strs3, str, index) == 1 || index3 != 0)
-            {
-                i--;
-            }
-            else
-            {
-                strs1[index3] = str;
-                index3++;
-            }
-            break;
-        }
-    }
-    FILE return 0;
-=======
         
         switch(strlen(str)){
             case 3 ... 8:
@@ -187,5 +107,4 @@ int main()
     }
     scriviSuFile(strs1, strs2, strs3);
     return 0;
->>>>>>> cebc28051e9ead002755f3caec0f6fcfba90cf29
 }
